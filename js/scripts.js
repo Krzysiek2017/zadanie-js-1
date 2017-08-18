9045359b72f4f526c188acc5d8755c5a50f90736
logan <musashimiyamoto@interia.eu>
2017-08-18 14:20:43 +0200

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-﻿var name = prompt('Enter your name');
-alert('Hello, ' + name);
+﻿// First comment
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+/* 
+    Second comment
+    comment
+*/
 console.log('Hello, ' + name);
